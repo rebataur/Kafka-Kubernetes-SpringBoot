c1410a6210bcaa96526ab65b397cc369605b85b9
Ranjan <pranjan24@dxc.com>
2021-08-19 16:37:15 +0530

    added readme

--- command-receiver/src/main/java/com/jromanmartin/kafka/streams/config/StreamsConfig.java
+++
@@ -1,9 +0,0 @@
-package com.jromanmartin.kafka.streams.config;
-
-import com.jromanmartin.kafka.streams.stream.GreetingsStreams;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(GreetingsStreams.class)
-public class StreamsConfig {
-    
-}
